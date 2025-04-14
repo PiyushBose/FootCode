2629413f7c4b34e2fee9d523803fa23445562b71
Piyush Bose <75084380+PiyushBose@users.noreply.github.com>
2025-04-14 15:14:11 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,3 +1,2 @@
-const url = "http://localhost"
-export const backendUrl = `${url}:3000`;
-export const proxyUrl = `${url}:5000`
+export const backendUrl = "https://footcode-backend.onrender.com";
+export const proxyUrl = "https://footcode-proxy.onrender.com";
